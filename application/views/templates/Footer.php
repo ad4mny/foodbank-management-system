2ca76edf7ee9c6f1fe6ab357a571091e24ce5c6b
adamz <58021562+ad4mny@users.noreply.github.com>
2021-08-18 20:46:25 +0800

    added zingchart js library

--- application/views/templates/Footer.php
+++ application/views/templates/Footer.php
@@ -6,8 +6,8 @@
     </div>
 </footer>
 
-<!-- Script Section -->
 <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
 
 </body>
