6198f3d2c146ec55feedd8c0adddd9d258dd917a
adamz <58021562+ad4mny@users.noreply.github.com>
2021-08-19 18:30:30 +0800

    added donation module

--- application/controllers/DonationController.php
+++ application/controllers/DonationController.php
@@ -5,14 +5,44 @@ class DonationController extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('DonationModel');
+        $this->load->model('FoodbankModel');
     }
 
     public function index()
     {
+        $data['donation'] = $this->getFoodbank();
+
         $this->load->view('templates/Header');
         $this->load->view('templates/Nav');
-        $this->load->view('Donation');
+        $this->load->view('Donation', $data);
         $this->load->view('templates/Footer');
+    }
+
+    public function getFoodbank()
+    {
+        $data = $this->FoodbankModel->getFoodbank();
+        // echo '<pre/>';
+        // var_dump($data);
+        // exit;
+        $item_id = '[';
+        $item_quantity = '[';
+        $item_name = '[';
+
+        if (isset($data) && is_array($data)) {
+            foreach ($data as $row) {
+                $item_id .= $row['item_id'] . ',';
+                $item_quantity .= $row['item_quantity'] . ',';
+                $item_name .= '\'' . $row['item_name'] . '\',';
+            }
+        }
+
+        $item_id .= ']';
+        $item_quantity .= ']';
+        $item_name .= ']';
+
+        $return = array(
+            $item_id, $item_quantity, $item_name
+        );
+        return $return;
     }
 }
