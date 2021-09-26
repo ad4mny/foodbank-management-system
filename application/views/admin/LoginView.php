550d84564214f4b90714bfdf47cd9bb64f03acb6
adamz <58021562+ad4mny@users.noreply.github.com>
2021-09-26 16:12:15 +0800

    minor ui updates to admin login interfaces

--- application/views/admin/LoginView.php
+++ application/views/admin/LoginView.php
@@ -1,9 +1,9 @@
 <div class="container p-5">
     <div class="row d-flex justify-content-center ">
-        <div class="col-4">
+        <div class="col-4 bg-white p-4 ">
             <form method="post" action="<?php echo base_url(); ?>login/verify">
                 <div class="form-group pb-2 text-center">
-                    <h3 class="">Admin Login</h3>
+                    <h3 class="text-dark fw-bold">Admin Login</h3>
                 </div>
                 <div class="form-group pb-2">
                     <input type="text" class="form-control" name="username" placeholder="Your username" required>
